0544c29b75c8d171c4be5356042c985647df4c78
RileyManda <rileymanda69@gmail.com>
2022-01-17 16:14:43 +0200

    loader component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RegisterComponent } from './components/register/register.component';
 import {MatCardModule } from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { DashnavbarComponent } from './components/dashnavbar/dashnavbar.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { DashnavbarComponent } from './components/dashnavbar/dashnavbar.componen
     ResetPasswordComponent,
     RegisterComponent,
     DashnavbarComponent,
+    LoaderComponent,
   ],
   imports: [
     BrowserModule,
