9da874ddbe6e6ef5156c946b81a0ad10f1786ef5
RileyManda <rileymanda69@gmail.com>
2022-01-27 21:21:37 +0200

    grid layout-ongoing fix.

--- src/app/components/sales-hours/sales-hours.component.ts
+++ src/app/components/sales-hours/sales-hours.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ChangeDetectorRef } from '@angular/core';
 import {ThemePalette} from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
-
+import {MediaMatcher} from '@angular/cdk/layout';
 
 export interface ChipColor {
   name: string;
-
 
 }
 @Component({
@@ -13,14 +12,31 @@ export interface ChipColor {
   templateUrl: './sales-hours.component.html',
   styleUrls: ['./sales-hours.component.css']
 })
-export class SalesHoursComponent implements OnInit {
+export class SalesHoursComponent {
+  mobileQuery: MediaQueryList;
 
-  constructor() { }
-  chips: ChipColor[] = [
+  fillerNav = Array(50).fill(0).map((_, i) => `Nav Item ${i + 1}`);
 
-    {name: 'Closed'}
-  ];
-  ngOnInit(): void {
+  closed = ["Closed", "Closed", "Closed", "Closed", "Closed", "Closed"]
+  open = ["Open", "Open","Open", "Open","Open", "Open"]
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 300px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 }
+
+
+
+
+
+
+
