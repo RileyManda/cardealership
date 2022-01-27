5822762e38a562fd5525bcd15edb1d9022f96f45
RileyManda <rileymanda69@gmail.com>
2022-01-27 18:40:45 +0200

    updated margins

--- src/app/views/otp-screen/otp-screen.component.ts
+++ src/app/views/otp-screen/otp-screen.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-otp-screen',
   templateUrl: './otp-screen.component.html',
-  styleUrls: ['./otp-screen.component.css']
+  styleUrls: ['./otp-screen.component.scss']
 })
 export class OtpScreenComponent implements OnInit {
 
